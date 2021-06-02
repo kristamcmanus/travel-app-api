0f9fba8aed9811b4f4224757a5a2440c411e4c16
kristamcmanus <k.mcmanus310@gmail.com>
2021-06-02 09:55:28 -0400

    Create post and comment models, routes, and curl-scripts

---
+++ app/models/post.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const commentSchema = require('./comment')
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  body: {
+    type: String,
+    required: true
+  },
+  comments: [commentSchema],
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
+},
+{
+  timestamps: true
+})
+module.exports = mongoose.model('Post', postSchema)
